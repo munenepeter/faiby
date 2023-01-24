0724f8cd3d464e3d61cbf620db44fa686f61d3cc
munenepeter <peternjega7@gmail.com>
2023-01-24 12:46:16 +0300

    prefix with admin

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::prefix('admin')->group(function () {
+
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+    });
 });
