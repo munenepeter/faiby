c7b702901ad758d192e3205ac78e400ae3745703
munenepeter <peternjega7@gmail.com>
2023-01-25 18:45:46 +0300

    added plans table

--- database/migrations/2023_01_24_153820_create_clients_table.php
+++ database/migrations/2023_01_24_153820_create_clients_table.php
@@ -1,25 +1,28 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string('full_names');
             $table->string('email')->unique();
             $table->string('phone');
-            $table->date('start_at');
-            $table->float('amount', 8, 2);
+            $table->unsignedInteger('plan_id');
+            $table->foreign('plan_id')->references('id')->on('plans');
+            $table->date('plan_start_at');
+            $table->date('plan_end_at')->default(
+                date("Y-m-d", strtotime('+1 month', strtotime(DB::raw('plan_start_at'))))
+            );
             $table->text('notes');
             $table->timestamps();
         });
@@ -30,8 +33,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('clients');
     }
 };
