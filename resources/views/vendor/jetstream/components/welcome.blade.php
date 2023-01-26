5f90107e9289cd5880d0a577abcff8309e0efe38
munenepeter <peternjega7@gmail.com>
2023-01-26 14:38:35 +0300

    dashboard uplift

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -1,17 +1,47 @@
-<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-    <div>
-        <x-jet-application-logo class="block h-12 w-auto" />
+<div class="p-4 sm:px-20 bg-gray-50 border-b border-gray-200">
+    <div class="flex flex-col">
+        <h2 class="mb-4 text-2xl font-bold">Overview</h2>
+        <div class="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div class="flex items-start rounded-xl bg-white p-4 shadow-lg">
+                <div class="flex h-12 w-12 items-center justify-center rounded-full border border-blue-100 bg-blue-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                    </svg>
+                </div>
+
+                <div class="ml-4">
+                    <h2 class="font-semibold">0 Invoices</h2>
+                    <p class="mt-2 text-sm text-gray-500">Last added 2 days ago</p>
+                </div>
+            </div>
+
+            <div class="flex items-start rounded-xl bg-white p-4 shadow-lg">
+                <div class="flex h-12 w-12 items-center justify-center rounded-full border border-orange-100 bg-orange-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-orange-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                    </svg>
+                </div>
+
+                <div class="ml-4">
+                    <h2 class="font-semibold">0 Users</h2>
+                    <p class="mt-2 text-sm text-gray-500">Last added 1 day ago</p>
+                </div>
+            </div>
+            <div class="flex items-start rounded-xl bg-white p-4 shadow-lg">
+                <div class="flex h-12 w-12 items-center justify-center rounded-full border border-red-100 bg-red-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                    </svg>
+                </div>
+
+                <div class="ml-4">
+                    <h2 class="font-semibold">2 Plans</h2>
+                    <p class="mt-2 text-sm text-gray-500">Last added 45 days ago</p>
+                </div>
+            </div>
+          
+        </div>
     </div>
 
-    <div class="mt-8 text-2xl">
-        Welcome to your Jetstream application!
-    </div>
 
-    <div class="mt-6 text-gray-500">
-        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-        ecosystem to be a breath of fresh air. We hope you love it.
-    </div>
 </div>
-
