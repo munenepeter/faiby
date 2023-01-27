e54d091783b8d81815018c3b4c63c3350633e6a8
munenepeter <peternjega7@gmail.com>
2023-01-27 17:18:55 +0300

    add payment mode

---
+++ database/migrations/2023_01_27_140202_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('period');
+            $table->float("amount", 8, 2);
+            $table->unsignedInteger('client_id');
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('payments');
+    }
+};
