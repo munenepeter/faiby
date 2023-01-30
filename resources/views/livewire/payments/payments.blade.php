2cbbeec222a75100658fe75ffa8dce3a8f9c346e
munenepeter <peternjega7@gmail.com>
2023-01-30 14:58:58 +0300

    css & payments

--- resources/views/livewire/payments/payments.blade.php
+++ resources/views/livewire/payments/payments.blade.php
@@ -1,8 +1,10 @@
-<div class="p-20 bg-green-200 text-green-800 grid grid-cols-3 gap-2">
-    <div class="bg-green-100 rounded p-5">card 1</div>
-    <div class="bg-green-100 rounded p-5">card 2</div>
-    <div class="bg-green-100 rounded p-5">card 3</div>
-    <div class="bg-green-100 rounded p-5">card 4</div>
-    <div class="bg-green-100 rounded p-5">card 5</div>
-    <div class="bg-green-100 rounded p-5">card 6</div>
-</div>
+<section>
+    <div class="p-8 bg-green-200 text-green-800 grid grid-cols-4 gap-2">
+        @for($i=1; $i<=12; $i++) <div class="bg-green-100 rounded p-5">{{date('M Y', mktime(0,0,0,$i, 1, date('Y')))}}</div>
+    @endfor
+    </div>
+    <div class="p-8 bg-green-200 text-blue-800 grid grid-cols-4 gap-2">
+        @for($i=1; $i<=12; $i++) <div class="bg-blue-100 rounded p-5">{{date('M Y', mktime(0,0,0,$i, 1, date('Y')))}}</div>
+    @endfor
+    </div>
+</section>
