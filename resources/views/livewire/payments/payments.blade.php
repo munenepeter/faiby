07214c4ec2b8447973e631537cebb728e8434db5
munenepeter <peternjega7@gmail.com>
2023-01-30 19:59:24 +0300

    new chnages

--- resources/views/livewire/payments/payments.blade.php
+++ resources/views/livewire/payments/payments.blade.php
@@ -1,12 +1,8 @@
 <section>
-    <div class="p-8 bg-green-200 text-green-800 grid grid-cols-4 gap-2">
-        @for($i=1; $i<=12; $i++) <div class="bg-green-100 rounded p-5">{{date('M Y', mktime(0,0,0,$i, 1, date('Y')))}}</div>
-    @endfor
-    </div>
     <div class="p-8 bg-green-200 text-blue-800 grid grid-cols-4 gap-2">
         @foreach($payments as $payment)
         <div class="bg-blue-100 rounded p-5">
-            {{$payment->period}}
+           <span> {{$payment->period}}</span>
         </div>
         @endforeach
     </div>
